aa89325302bf26ae509579e96738053f1e50488f
qchau97 <vtquynhchau@gmail.com>
2021-02-14 07:03:54 +0700

    Finishing the Merged Form & Input Management

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -12,21 +12,34 @@ const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
 const formReducer = (state, action) => {
   switch (action.type) {
     case FORM_INPUT_UPDATE:
+      const updatedInputValues = {
+        ...state.inputValues,
+        [action.payload.inputLabel]: action.payload.value
+      }
+      const updatedInputValidities = {
+        ...state.inputValidities,
+        [action.payload.inputLabel]: action.payload.isInputValid
+      }
+      let updatedIsFormValid = true;
+      for (const key in updatedInputValidities) {
+        updatedIsFormValid = updatedIsFormValid && updatedInputValidities[key]
+      }
+      return {
+        ...state,
+        inputValues: updatedInputValues,
+        inputValidities: updatedInputValidities,
+        isFormValid: updatedIsFormValid
+      }
+    default:
+      return state;
   }
-}
+};
 
 const EditProductScreen = ({ navigation }) => {
   const dispatch = useDispatch();
   const editedProductId = navigation.getParam('productId');
   const editedProduct = useSelector(state => state.products.userProducts.find(product => product.id === editedProductId));
 
-  // const handlePriceChange = (newPrice) => {
-  //   if (!editedProduct) {
-  //     setPrice(newPrice);
-  //   }
-  // };
-
-  // Using useReducer instead of useState for state management
   // const [state, dispatch] = useReducer(reducer, initialArg, init);
   const [formState, dispatchFormState] = useReducer(formReducer, {
     inputValues: {
@@ -47,19 +60,29 @@ const EditProductScreen = ({ navigation }) => {
   // useCallback ensures that handleFormSubmit function is not re-created when component re-renders
   // Therefore avoid entering infinite loop
   const handleFormSubmit = useCallback(() => {
-    if (!isTitleValid) {
+    if (!formState.isFormValid) {
       Alert.alert('Wrong input!', 'Please check the errors in the form.', [
         { text: 'OK' }
       ]);
       return;
     }
     if (!editedProduct) {
-      dispatch(createProduct(title, description, +price));
+      dispatch(createProduct(
+        formState.inputValues.title,
+        // formState.inputValues.imageUrl,
+        formState.inputValues.description,
+        +formState.inputValues.price
+      ));
     } else {
-      dispatch(updateProduct(editedProductId, title, description));
+      dispatch(updateProduct(
+        editedProductId,
+        formState.inputValues.title,
+        // formState.inputValues.imageUrl,
+        formState.inputValues.description,
+      ));
     }
     navigation.goBack();
-  }, [isTitleValid, dispatch, editedProductId, title, description, price]);
+  }, [dispatch, editedProductId, formState]);
 
   useEffect(() => {
     navigation.setParams({
@@ -67,18 +90,18 @@ const EditProductScreen = ({ navigation }) => {
     });
   }, [handleFormSubmit]);
 
-  const handleTitleChange = (text) => {
-    let isTitleValid = false;
-    if (text.trim().length > 0) {
-      isTitleValid = true;
+  const handleInputChange = (inputLabel, newInput) => {
+    let isInputValid = false;
+    if (newInput.trim().length > 0) {
+      isInputValid = true;
     }
     dispatchFormState({
       type: FORM_INPUT_UPDATE,
       payload: {
-        value: text,
-        isTitleValid: isTitleValid,
-        input: 'title', // let our reducer know which input triggers this
-      }    
+        value: newInput,
+        isInputValid: isInputValid,
+        inputLabel: inputLabel,
+      }
     })
   };
 
@@ -89,29 +112,34 @@ const EditProductScreen = ({ navigation }) => {
           <Text style={styles.label}>Title</Text>
           <TextInput
             style={styles.input}
-            value={title}
-            onChangeText={handleTitleChange}
+            value={formState.inputValues.title}
+            onChangeText={(text) => handleInputChange('title', text)}
+            // 'newInput' is default argument which React Native would pass it automatically as the last argument in 'handleInputChange' function
             keyboardType='default'
             autoCapitalize='sentences'
             autoCorrect
             returnKeyType='next'
           />
         </View>
-        {!isTitleValid && <Text>Please enter a valid title!</Text>}
+        {!formState.inputValidities.title && <Text>Please enter a valid title!</Text>}
         {/* <View style={styles.formControl}>
           <Text style={styles.label}>Image URL</Text>
           <TextInput
             style={styles.input}
-            value={imageUrl}
-            onChangeText={text => setImageUrl(text)}
+            value={formState.inputValues.imageUrl}
+            onChangeText={(text) => handleInputChange('imageUrl', text)}
           />
         </View> */}
         <View style={styles.formControl}>
           <Text style={styles.label}>Price</Text>
           <TextInput
             style={styles.input}
-            value={price}
-            onChangeText={handlePriceChange}
+            value={formState.inputValues.price}
+            onChangeText={(text) => {
+              if (!editedProduct) {
+                handleInputChange('price', text)
+              }
+            }}
             keyboardType='decimal-pad'
           />
         </View>
@@ -119,8 +147,8 @@ const EditProductScreen = ({ navigation }) => {
           <Text style={styles.label}>Description</Text>
           <TextInput
             style={styles.input}
-            value={description}
-            onChangeText={text => setDescription(text)}
+            value={formState.inputValues.description}
+            onChangeText={(text) => handleInputChange('description', text)}
             keyboardType='default'
             autoCapitalize='sentences'
           />
