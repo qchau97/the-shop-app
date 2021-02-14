e2cae759d409ff235d6e15fc1a35f7a61a7977b2
qchau97 <vtquynhchau@gmail.com>
2021-02-14 16:06:36 +0700

    Installing Redux Thunk

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 import ShopNavigator from './navigation/ShopNavigator';
 import cartReducer from './store/reducers/cartReducer';
 import ordersReducer from './store/reducers/ordersReducer';
 import productsReducer from './store/reducers/productsReducer';
+import ReduxThunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -13,7 +14,7 @@ const rootReducer = combineReducers({
   orders: ordersReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const App = () => {
   return (
